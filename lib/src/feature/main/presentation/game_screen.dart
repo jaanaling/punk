b688077faf1b39ecf365b11f672b4a2a53721f68
Jaanaling <vvv97914002@gmail.com>
2025-04-17 21:56:02 +0300

    fix

--- lib/src/feature/main/presentation/game_screen.dart
+++ lib/src/feature/main/presentation/game_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:the_eye_of_the_world/src/core/utils/animated_button.dart';
+import 'package:the_eye_of_the_world/src/core/utils/app_icon.dart';
+import 'package:the_eye_of_the_world/src/core/utils/icon_provider.dart';
 import 'package:the_eye_of_the_world/src/core/utils/size_utils.dart';
 import 'package:the_eye_of_the_world/src/feature/main/bloc/app_bloc.dart';
 import 'package:the_eye_of_the_world/src/feature/main/model/model.dart';
@@ -167,34 +173,11 @@ class _GameScreenState extends State<GameScreen> {
                 height: double.infinity,
               ),
               // Dossier Button
-              Positioned(
-                top: 20,
-                right: 20,
-                child: ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => _buildDossierDialog(state, context),
-                    );
-                  },
-                  child: const Text('Dossier'),
-                ),
-              ),
 
               if (firstParticipantDossier != null && _speakerId != 'NARRATOR')
                 Positioned(
-                  left: 20,
-                  bottom: 20,
-                  child: CharacterAvatar(
-                    character: firstParticipantDossier!,
-                    isDimmed: isFirstDimmed,
-                    emotion: firstParticipantEmotionPath,
-                  ),
-                ),
-              if (firstParticipantDossier != null && _speakerId != 'NARRATOR')
-                Positioned(
-                  left: 20, // 🔸 слева
-                  bottom: 20,
+                  left: 70, // 🔸 слева
+                  bottom: 0,
                   child: CharacterAvatar(
                     character: firstParticipantDossier!,
                     isDimmed: isFirstDimmed,
@@ -203,8 +186,8 @@ class _GameScreenState extends State<GameScreen> {
                 ),
               if (secondParticipantDossier != null && _speakerId != 'NARRATOR')
                 Positioned(
-                  left: 190, // 🔸 тоже слева, чуть правее первого
-                  bottom: 20,
+                  left: 350, // 🔸 тоже слева, чуть правее первого
+                  bottom: 0,
                   child: CharacterAvatar(
                     character: secondParticipantDossier!,
                     isDimmed: isSecondDimmed,
@@ -219,6 +202,8 @@ class _GameScreenState extends State<GameScreen> {
                 child: DialogueBox(
                   phrase: currentPhrase,
                   choices: currentBranch.choices,
+                  state: state,
+                  mood: currentBranch.heroMentalState,
                   onTap: (DialogueChoice choice) {
                     context.read<DialogueBloc>().add(OptionChosenEvent(choice));
                     _currentPhraseIndex = 0;
@@ -240,32 +225,126 @@ class _GameScreenState extends State<GameScreen> {
   }
 }
 
-Widget _buildDossierDialog(DialogueState state, BuildContext context) {
-  return AlertDialog(
-    title: const Text('Dossier'),
-    content: SizedBox(
-      width: double.maxFinite,
-      child: ListView.builder(
-        shrinkWrap: true,
-        itemCount: state.gameState.dossiers.length,
-        itemBuilder: (context, index) {
-          final dossier = state.gameState.dossiers[index];
-          return ListTile(
-            title: Text(dossier.name),
-            subtitle: Text(dossier.background),
-            // You can add more details or customize the display here
-          );
-        },
-      ),
-    ),
-    actions: [
-      TextButton(
-        onPressed: () {
-          context.pop();
-        },
-        child: const Text('Close'),
-      ),
-    ],
+void _buildDossierDialog(DialogueState state, BuildContext context) {
+  int currentIndex = 0;
+
+  showDialog(
+    context: context,
+    barrierColor: Colors.black.withOpacity(0.5),
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return StatefulBuilder(
+        builder: (context, setState) => Dialog(
+          backgroundColor: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AnimatedButton(
+                    onPressed: () {
+                      if (currentIndex > 0) {
+                        setState(() {
+                          currentIndex--;
+                        });
+                      }
+                    },
+                    child: AppIcon(
+                      asset: "assets/images/back.webp",
+                      width: 64,
+                    ),
+                  ),
+                  Gap(10),
+                  Container(
+                    width: getWidth(context, percent: 0.7),
+                    height: getHeight(context, percent: 0.6),
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/dossier_panel.webp'))),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/${state.gameState.dossiers[currentIndex].name} dossier.webp',
+                          height: getHeight(context, percent: 0.5),
+                          fit: BoxFit.contain,
+                        ),
+                        SingleChildScrollView(
+                          child: SizedBox(
+                   
+                            width: getWidth(context, percent: 0.4),
+                            child: Column(
+                              children: [
+                                Text(
+                                  state.gameState.dossiers[currentIndex].name,
+                                  style: const TextStyle(
+                                      color: Color.fromARGB(255, 122, 210, 241),
+                                      fontSize: 14,
+                                      fontFamily: "Orbitron"),
+                                ),
+                                Text(
+                                  state.gameState.dossiers[currentIndex]
+                                      .background,
+                                  style: const TextStyle(
+                                      color: Color(0xFFB3EBFF),
+                                      fontSize: 12,
+                                      fontFamily: "Orbitron"),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Gap(10),
+                  AnimatedButton(
+                    onPressed: () {
+                      if (currentIndex < state.gameState.dossiers.length - 1) {
+                        setState(() {
+                          currentIndex++;
+                        });
+                      }
+                    },
+                    child: Transform.rotate(
+                      angle: 3.14,
+                      child: AppIcon(
+                        asset: "assets/images/back.webp",
+                        width: 64,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              AnimatedButton(
+                onPressed: () {
+                  context.pop();
+                },
+                child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                        color: const Color(0x00000075),
+                        border: Border.all(
+                          color: const Color(0xFFFFFF00),
+                          width: 2.0,
+                        )),
+                    child: Text(
+                      "Close",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Color(0xFFB2FF00),
+                          fontSize: 16,
+                          fontFamily: "Jersey25"),
+                    )),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
   );
 }
 
@@ -303,13 +382,23 @@ class CharacterAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Opacity(
-      opacity: isDimmed ? 0.5 : 1.0,
-      child: Image.asset(
-        emotion,
-        width: 150,
-        height: 200,
-        fit: BoxFit.contain,
+    return Transform.translate(
+      offset: Offset(0, isDimmed ? 0 : 0),
+      child: Transform.scale(
+        scale: isDimmed ? 1 : 1.5,
+        child: Opacity(
+          opacity: isDimmed ? 0.8 : 1.0,
+          child: Image.asset(
+            emotion,
+            height: getHeight(context, percent: 0.7),
+            fit: BoxFit.contain,
+            errorBuilder: (context, error, stackTrace) => Image.asset(
+              'assets/images/${character.name} neutral.webp',
+              height: getHeight(context, percent: 0.7),
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -320,11 +409,15 @@ class DialogueBox extends StatefulWidget {
   final List<DialogueChoice> choices; // добавили
   final VoidCallback onDialogueTap;
   final Function(DialogueChoice) onTap;
+  final DialogueState state;
+  final String mood;
   const DialogueBox({
     required this.phrase,
     required this.choices,
     required this.onDialogueTap,
+    required this.state,
     required this.onTap,
+    required this.mood,
     Key? key,
   }) : super(key: key);
 
@@ -333,15 +426,37 @@ class DialogueBox extends StatefulWidget {
 }
 
 class _DialogueBoxState extends State<DialogueBox> {
+  final ScrollController _scrollController = ScrollController(); // 👈 ①
+  double _scrollProgress = 0; // 👈 ②
   int _visibleTextLength = 0;
   bool _isTextFullyVisible = false;
 
   @override
   void initState() {
     super.initState();
-    _visibleTextLength = 0;
-    _isTextFullyVisible = false;
     _startTextAnimation();
+
+    // 👇 слушаем позицию прокрутки и считаем прогресс
+    _scrollController.addListener(_handleScroll); // 👈 ③
+  }
+
+  void _handleScroll() {
+    // позиция может быть 0, пока виджет ещё не отрисовался
+    if (!_scrollController.hasClients ||
+        !_scrollController.position.hasContentDimensions) return;
+
+    final max = _scrollController.position.maxScrollExtent;
+    final offset = _scrollController.offset;
+    setState(() {
+      _scrollProgress = max == 0 ? 0 : (offset / max).clamp(0.0, 1.0);
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_handleScroll);
+    _scrollController.dispose(); // 👈 ④
+    super.dispose();
   }
 
   void _startTextAnimation() {
@@ -376,9 +491,36 @@ class _DialogueBoxState extends State<DialogueBox> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
+    Color textColor = Colors.white;
+    if (widget.phrase != null) {
+      lastmood = widget.mood;
+      switch (widget.phrase!.speakerId) {
+        case 'VAIR':
+          textColor = Colors.green;
+          break;
+        case 'ERRAS':
+          textColor = Colors.purple;
+          break;
+        case 'LYRA':
+          textColor = Colors.blue;
+          break;
+        case 'NIKA':
+          textColor = Colors.orange;
+          break;
+        case 'RION':
+          textColor = Colors.red;
+          break;
+        case 'NARRATOR':
+          textColor = Colors.grey.shade300;
+          break;
+        default:
+          textColor = Colors.white;
+      }
+    }
+
     return SizedBox(
       // 👉 занимает всю высоту
-      width: screenWidth * 0.30, // 👉 30 % ширины
+      width: screenWidth * 0.35, // 👉 30 % ширины
       height: screenHeight,
       child: GestureDetector(
         onTap: () {
@@ -404,39 +546,125 @@ class _DialogueBoxState extends State<DialogueBox> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // ---------- 1. Текст фразы ----------
-              if (widget.phrase != null) ...[
-                Text(
-                  widget.phrase!.speakerId,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+              if (widget.phrase != null)
+                SizedBox(
+                  height: screenHeight * 0.5,
+                  child: Scrollbar(
+                    controller: _scrollController,
+                    thumbVisibility: true,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (widget.phrase != null) ...[
+                            Text(
+                              widget.phrase!.speakerId,
+                              style: TextStyle(
+                                color: textColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                fontFamily: "Jersey25",
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              widget.phrase!.text
+                                  .substring(0, _visibleTextLength),
+                              style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontFamily: "Orbitron"),
+                            ),
+                          ],
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                const SizedBox(height: 8),
-                Text(
-                  widget.phrase!.text.substring(0, _visibleTextLength),
-                  style: const TextStyle(fontSize: 16, color: Colors.white),
-                ),
-                const Spacer(), // 👉 кнопки будут «прилипать» к низу
-              ],
 
               // ---------- 2. Кнопки выбора ----------
               if (widget.phrase == null && widget.choices.isNotEmpty)
                 ...widget.choices.map(
                   (choice) => Padding(
                     padding: const EdgeInsets.only(bottom: 8.0),
-                    child: ElevatedButton(
+                    child: AnimatedButton(
                       onPressed: () {
                         widget.onTap(choice);
                         _visibleTextLength = 0;
                         _isTextFullyVisible = false;
                         _startTextAnimation();
                       },
-                      child: Text(choice.choiceText),
+                      child: Container(
+                          padding: const EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                              color: const Color(0x00000075),
+                              border: Border.all(
+                                color: const Color(0xFFFFFF00),
+                                width: 2.0,
+                              )),
+                          child: Text(
+                            choice.choiceText,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: [
+                                  Color(0xFFB2FF00),
+                                  Color(0xFFFFBB00),
+                                  Color(0xFFFF4D00),
+                                ][Random().nextInt(3)],
+                                fontSize: 16,
+                                fontFamily: "Jersey25"),
+                          )),
                     ),
                   ),
                 ),
+
+              Spacer(),
+              Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                      color: const Color(0x00000075),
+                      border: Border.all(
+                        color: const Color(0xFFFFFF00),
+                        width: 2.0,
+                      )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Column(
+                        children: [
+                          AppIcon(
+                              asset: "assets/images/$lastmood.webp",
+                              width: 110),
+                          Text(
+                            lastmood,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontFamily: "Jersey25"),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          AnimatedButton(
+                              onPressed: () =>
+                                  _buildDossierDialog(widget.state, context),
+                              child: AppIcon(
+                                asset: "assets/images/dossier.webp",
+                                width: 64,
+                                height: 70,
+                              )),
+                          AnimatedButton(
+                              onPressed: () => context.pop(),
+                              child: AppIcon(
+                                asset: "assets/images/MENU.webp",
+                                width: 91,
+                              )),
+                        ],
+                      )
+                    ],
+                  )),
             ],
           ),
         ),
@@ -444,3 +672,5 @@ class _DialogueBoxState extends State<DialogueBox> {
     );
   }
 }
+
+String lastmood = "";
